6e9df9d1f41e970ca2f3c7ee507f37f516316b4b
Apple <teodikabrami@gmail.com>
2018-10-03 15:43:37 +0330

    Initial Commit

---
+++ GravityAnimatorSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GravityAnimatorSwift
+//
+//  Created by Teodik Abrami on 10/3/18.
+//  Copyright © 2018 Teodik Abrami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
